90b61f8ae1234c796a54520954475d4db605fbd0
apad2025 <78267247+apad2025@users.noreply.github.com>
2023-04-24 19:13:23 -0500

    Memory
    App remembers date you selected after being closed

--- Streaks/ContentView.swift
+++ Streaks/ContentView.swift
@@ -11,6 +11,16 @@ struct ContentView: View {
     @State private var specifiedDate = Date()
     @State private var showingDatePicker = false
     
+    private func saveDate() {
+        UserDefaults.standard.set(specifiedDate, forKey: "specifiedDate")
+    }
+
+    private func loadDate() {
+        if let savedDate = UserDefaults.standard.object(forKey: "specifiedDate") as? Date {
+            specifiedDate = savedDate
+        }
+    }
+    
     var body: some View {
         ZStack {
             VStack {
@@ -18,21 +28,34 @@ struct ContentView: View {
                 Button("Change Date") {
                     showingDatePicker = true
                 }
-                .sheet(isPresented: $showingDatePicker) {
-                    DatePickerView(selectedDate: $specifiedDate)
-                }
                 .padding(.bottom)
             }
+            
             VStack {
-                Text("🔥")
-                    .font(.system(size: 80))
+                Spacer(minLength: 0)
                 
-                Text(daysElapsedText + " days")
-                    .font(.system(size: 80, design: .rounded))
-                    .bold()
+                VStack {
+                    Text("🔥")
+                        .font(.system(size: 80))
+                    
+                    Text(daysElapsedText + " days")
+                        .font(.system(size: 80, design: .rounded))
+                        .bold()
+                    
+                    Text("Let's keep it going!")
+                }
                 
-                Text("Let's keep it going!")
+                Spacer(minLength: 0)
             }
+        }
+        .sheet(isPresented: $showingDatePicker) {
+            DatePickerView(selectedDate: $specifiedDate)
+                .onDisappear {
+                    saveDate()
+                }
+        }
+        .onAppear {
+            loadDate()
         }
     }
     
